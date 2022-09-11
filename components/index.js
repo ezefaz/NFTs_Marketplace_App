88c8b37b6238704fd1f3a937e83733844ffc0817
ezefaz <ezefaz@gmail.com>
2022-09-11 13:00:39 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import FocusedStatusBar from './FocusedStatusBar';
+import DetailsBid from './DetailsBid';
+import DetailsDesc from './DetailsDesc';
+import { SubInfo } from './SubInfo';
+import { CircleButton, RectButton } from './Button';
+
+export {
+    NFTCard, HomeHeader, FocusedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo
+}
